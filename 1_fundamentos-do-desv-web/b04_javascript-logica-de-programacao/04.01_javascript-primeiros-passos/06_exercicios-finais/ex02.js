2788e6b383a9188e3c07c03a33c2084ad9a6c840
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 20:33:21 -0300

    Exercícios finais - ex02 concluído

---
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex02.js
@@ -0,0 +1,12 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. 
+
+const a = 8;
+const b = 5;
+
+if (a > b) {
+    console.log(a);
+} else if (a < b) {
+    console.log(b);
+} else if (a == b) {
+    console.log('Eles são iguais')
+}
