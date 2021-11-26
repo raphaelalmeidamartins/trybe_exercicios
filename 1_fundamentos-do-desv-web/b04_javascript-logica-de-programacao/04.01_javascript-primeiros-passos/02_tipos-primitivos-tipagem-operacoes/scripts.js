d54e3451b90335e79a2d4f9a38ad6e200e6dab21
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 16:17:24 -0300

    Adicionei exercício tipos primitivos, tipagem dinâmica e operações aritiméticas

---
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/02_tipos-primitivos-tipagem-operacoes/scripts.js
@@ -0,0 +1,15 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+// quando a variável não foi declarada, o comando typeof retorna 'undefined' (indefinido)
+console.log(typeof patientAge);
