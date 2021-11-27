a0f7c54e85955a42b3b01ca953a21449347cbfd5
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 23:07:06 -0300

    ex07 concluído

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex07.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex07.js
@@ -7,3 +7,20 @@
 // - Porcentagem < 50 -> F
 // - O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
 
+const nota = -100;
+
+if (nota >= 90 && nota < 100) {
+    console.log("A");
+} else if (nota >= 80 && nota < 90) {
+    console.log("B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("C");
+} else if (nota >= 60 && nota < 70) {
+    console.log("D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("F");
+} else if (nota > 100 || nota < 0) {
+    console.log("ERRO: nota inválida. Insira um valor entre 0 e 100.");
+}
