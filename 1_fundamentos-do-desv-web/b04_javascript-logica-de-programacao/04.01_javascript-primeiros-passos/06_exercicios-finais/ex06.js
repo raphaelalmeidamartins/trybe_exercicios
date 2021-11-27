db00e3a3a3f81b47e9f27adcd8dca2f0316a8d73
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 22:59:47 -0300

    Concluí ex06

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex06.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex06.js
@@ -3,3 +3,28 @@
 // - Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
 // - Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 // - Exemplo: bishop (bispo) -> diagonals (diagonais)
+
+let peca = 'Bispo';
+
+switch (peca.toLowerCase()) {
+    case 'peão':
+        console.log('PEÃO: apenas para frente, não pode retornar')
+        break;
+    case 'cavalo':
+        console.log('CAVALO: se move em L.');
+        break;
+    case 'bispo':
+        console.log('BISPO: apenas diagonais.');
+        break;
+    case 'torre':
+        console.log('TORRE: apenas horizontais e verticais.');
+        break;
+    case 'rainha':
+        console.log('RAINHA: horizontais, verticais e diagonais.');
+        break;
+    case 'rei':
+        console.log('REI: horizontais, verticais e diagonais, mas apenas uma casa por turno.')
+    default:
+        console.log('ERRO: peça inválida');
+        break;
+}
