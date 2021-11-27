1a7d326633caac821531ce275cc04f38a0b5179b
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 22:37:47 -0300

    Agora sim conclui ex04

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex04.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex04.js
@@ -1,2 +1,11 @@
 // Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. 
 
+const num = -5;
+
+if (num > 0) {
+    console.log('positive');
+} else if (num < 0) {
+    console.log('negative');
+} else if (num == 0) {
+    console.log('zero');
+}
