de6f252f474b507141e5925562bb47f05e5fc924
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-27 00:02:26 -0300

    ex11 concluído

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex11.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex11.js
@@ -11,3 +11,32 @@
 //         De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
 //         De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
 //         Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
+const salarioBruto = 3000;
+
+let salarioBase;
+let salarioLiquido; 
+
+if (salarioBruto <= 1566.94) {
+    salarioBase = salarioBruto - (salarioBruto * 0.08);
+} else if (salarioBruto > 1566.94 && salarioBruto <= 2594.92) {
+    salarioBase = salarioBruto - (salarioBruto * 0.09);
+} else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82) {
+    salarioBase = salarioBruto - (salarioBruto * 0.11);
+} else if (salarioBruto > 5189.82) {
+    salarioBase = salarioBruto - 570.88;
+}
+
+if (salarioBase <= 1903.98) {
+    salarioLiquido = salarioBase;
+} else if (salarioBase > 1903.98 && salarioBase <= 2826.65) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.075) - 142.80);
+} else if (salarioBase > 2826.65 && salarioBase <= 3751.05) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.15) - 354.80);
+} else if (salarioBase > 3751.05 && salarioBase <= 4664.68) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.225) - 636.13);
+} else if (salarioBase > 4664.68) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.275) - 869.36);
+}
+
+console.log(salarioLiquido);
