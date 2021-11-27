f6cdaef66b5a0b4ddd47caedcfcfedd5c4632d8a
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 23:18:07 -0300

    Concluí ex10

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex10.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex10.js
@@ -4,3 +4,15 @@
 // - O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
 //     - valorCustoTotal = valorCusto + impostoSobreOCusto
 //     - lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+const valorVenda = 1000;
+const valorCusto = 400;
+let impostoSobreOCusto = 400 * 0.20;
+let valorCustoTotal = valorCusto + impostoSobreOCusto;
+let lucro = valorVenda - valorCustoTotal;
+
+if (valorCusto < 0 || valorCusto < 0 ) {
+    console.log("ERRO: valores inválidos. Os valores de custo e venda precisam ser maiores que 0.")
+} else {
+    console.log(lucro);
+}
