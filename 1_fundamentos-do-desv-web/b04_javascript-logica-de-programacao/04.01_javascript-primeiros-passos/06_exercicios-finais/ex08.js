0cae34d6c28de89b2c77d45bc1a97b9436939211
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-26 23:10:18 -0300

    ex08 concluído

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex08.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.01_javascript-primeiros-passos/06_exercicios-finais/ex08.js
@@ -1,2 +1,12 @@
 // Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
 // Bonus: use somente um if .
+
+const a = 1;
+const b = 2;
+const c = 12;
+
+if ((a % 2) == 0 || (b % 2) == 0 || (c % 2) == 0 ) {
+    console.log('true');
+} else {
+    console.log('false');
+}
