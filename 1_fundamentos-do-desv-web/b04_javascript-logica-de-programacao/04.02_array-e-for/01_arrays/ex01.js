8a5b80041a4e057985ea414ca39528b25c4e4560
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-11-29 15:54:26 -0300

    Exercícios dia 04.02 - Incluí exercícios de fixação sobre arrays.

---
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.02_array-e-for/01_arrays/ex01.js
@@ -0,0 +1,7 @@
+// Obtenha o valor "Serviços" do array menu: 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
---
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.02_array-e-for/01_arrays/ex02.js
@@ -0,0 +1,6 @@
+// Procure o índice do valor "Portfólio" do array menu: 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
