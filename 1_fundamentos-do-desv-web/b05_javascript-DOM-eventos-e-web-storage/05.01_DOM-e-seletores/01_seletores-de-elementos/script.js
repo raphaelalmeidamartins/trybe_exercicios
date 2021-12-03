258dbe8dd1d42bc8a36410dfbc8c74a4625d7fe3
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-12-03 18:57:18 -0300

    Adicionei exercícios do dia 05.01 - 03/12/2021 - Bloco 05

---
+++ 1_fundamentos-do-desv-web/b05_javascript-DOM-eventos-e-web-storage/05.01_DOM-e-seletores/01_seletores-de-elementos/script.js
@@ -0,0 +1,14 @@
+//meu código aqui
+document.getElementById('header-container').style.backgroundColor = '#00b069';
+
+document.querySelector('#footer-container').style.backgroundColor = '#003533';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = '#a500f3';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = '#a500f3';
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = '#232525';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = '#232525';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = '#ff9f84';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = '#f9db5e';
