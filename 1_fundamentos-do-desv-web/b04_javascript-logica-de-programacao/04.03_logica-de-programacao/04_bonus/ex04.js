e1d2e268514a0088539c2f9814134bf1af752ddd
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-12-03 22:21:23 -0300

    Resolvi exercício bônus 4

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.03_logica-de-programacao/04_bonus/ex04.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.03_logica-de-programacao/04_bonus/ex04.js
@@ -1,17 +1,23 @@
-// Depois, faça uma pirâmide com n asteriscos de base: 
+//Depois, faça uma pirâmide com n asteriscos de base: 
+//Me inspirei neste vídeo para resolver esse exercício https://youtu.be/xzstcj3Cuso
 
 let n = 5;
-let centroN = (n+1)/2;
-let linha = [];
-let colunas = [];
-let symbol = "*";
+let centroN = (n + 1)/2;
+let stringLinha = '';
 
-for (let iColuna = 0; iColuna < centroN; iColuna += 1) {
-  for (let iLinha = 0; iLinha < n; iLinha += 1) {
-    linha.push(symbol);
+for (let linha = 0; linha < centroN; linha += 1) {
+  for (let coluna = 1; coluna < (centroN - linha); coluna += 1) {
+    stringLinha += ' ';
   }
-  colunas[iColuna] = linha;
-  linha = [];
+  for (let coluna = 0; coluna <= linha; coluna += 1) {
+    stringLinha += '*';
+  }
+  for (let coluna = 0; coluna < linha; coluna += 1) {
+    stringLinha += '*';
+  }
+  for (let coluna = 1; coluna < (centroN - linha); coluna += 1) {
+    stringLinha += ' ';
+  }
+  console.log(stringLinha);
+  stringLinha = '';
 }
-
-console.log(colunas);
