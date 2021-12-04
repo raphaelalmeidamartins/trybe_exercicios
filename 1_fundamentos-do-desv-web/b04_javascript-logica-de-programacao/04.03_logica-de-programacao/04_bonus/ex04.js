de24fd90eff3e84ce4e33ead13620829908bf2a4
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-12-03 22:25:54 -0300

    Complementei exercício bônus 4

--- 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.03_logica-de-programacao/04_bonus/ex04.js
+++ 1_fundamentos-do-desv-web/b04_javascript-logica-de-programacao/04.03_logica-de-programacao/04_bonus/ex04.js
@@ -1,23 +1,27 @@
 //Depois, faça uma pirâmide com n asteriscos de base: 
 //Me inspirei neste vídeo para resolver esse exercício https://youtu.be/xzstcj3Cuso
 
-let n = 5;
+let n = 11;
 let centroN = (n + 1)/2;
 let stringLinha = '';
 
-for (let linha = 0; linha < centroN; linha += 1) {
-  for (let coluna = 1; coluna < (centroN - linha); coluna += 1) {
-    stringLinha += ' ';
+if (n % 2 === 0) {
+  console.log('O número precisa ser ímpar');
+} else {
+  for (let linha = 0; linha < centroN; linha += 1) {
+    for (let coluna = 1; coluna < (centroN - linha); coluna += 1) {
+      stringLinha += ' ';
+    }
+    for (let coluna = 0; coluna <= linha; coluna += 1) {
+      stringLinha += '*';
+    }
+    for (let coluna = 0; coluna < linha; coluna += 1) {
+      stringLinha += '*';
+    }
+    for (let coluna = 1; coluna < (centroN - linha); coluna += 1) {
+      stringLinha += ' ';
+    }
+    console.log(stringLinha);
+    stringLinha = '';
   }
-  for (let coluna = 0; coluna <= linha; coluna += 1) {
-    stringLinha += '*';
-  }
-  for (let coluna = 0; coluna < linha; coluna += 1) {
-    stringLinha += '*';
-  }
-  for (let coluna = 1; coluna < (centroN - linha); coluna += 1) {
-    stringLinha += ' ';
-  }
-  console.log(stringLinha);
-  stringLinha = '';
 }
