07e10e9e4ec971822228cb54e055e3bed8b0f9eb
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-12-07 10:37:55 -0300

    Exercícios de fixação parte I - Buscando Elementos

--- 1_fundamentos-do-desv-web/b05_javascript-DOM-eventos-e-web-storage/05.02_JavaScript-trabalhando-com-elementos/01_buscando-elementos/script.js
+++ 1_fundamentos-do-desv-web/b05_javascript-DOM-eventos-e-web-storage/05.02_JavaScript-trabalhando-com-elementos/01_buscando-elementos/script.js
@@ -1,7 +1,23 @@
-let pai = document.getElementById('elementoOndeVoceEsta').parentElement;
-pai.style.color('green');
+// 1 - Acesse o elemento elementoOndeVoceEsta . 
+console.log(document.getElementById('elementoOndeVoceEsta').parentElement);
 
-let primeiroFilho = pai.firstChild;
+// 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele. 
+document.getElementById('elementoOndeVoceEsta').parentElement.style.color = 'red';
 
-firstChild.innerText()
+// 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+document.getElementById('primeiroFilhoDoFilho').innerHTML = '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae ipsum corporis distinctio adipisci eum temporibus quia sunt eos ipsam nostrum, earum mollitia accusamus recusandae minus necessitatibus consequuntur molestias iure. Facere?</p>'
 
+// 4 - Acesse o primeiroFilho a partir de pai.
+console.log(document.getElementById('pai').firstElementChild);
+
+// 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta . 
+console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
+
+// 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+
+// 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling.nextSibling);
+
+// 8 - Agora acesse o terceiroFilho a partir de pai .
+console.log(document.getElementById('pai').lastElementChild.previousElementSibling);
