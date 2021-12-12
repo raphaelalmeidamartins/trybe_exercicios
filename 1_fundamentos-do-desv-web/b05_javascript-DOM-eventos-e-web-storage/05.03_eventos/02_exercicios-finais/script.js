d46faf7ea40bd5f2a438cc3b5df38d99fcbb6e22
Raphael Martins <raphael.almeida.martins@gmail.com>
2021-12-12 16:24:01 -0300

    Comecei os exercícios finais

---
+++ 1_fundamentos-do-desv-web/b05_javascript-DOM-eventos-e-web-storage/05.03_eventos/02_exercicios-finais/script.js
@@ -0,0 +1,18 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
