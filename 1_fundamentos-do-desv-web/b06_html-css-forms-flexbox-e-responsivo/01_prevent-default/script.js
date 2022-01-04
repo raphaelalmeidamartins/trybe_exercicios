6b22cbbfed1b1120f1cdd17fa57c7c95f6290219
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-04 16:16:02 -0300

    06.01 - Para fixar parte IV

--- 1_fundamentos-do-desv-web/b06_html-css-forms-flexbox-e-responsivo/01_prevent-default/script.js
+++ 1_fundamentos-do-desv-web/b06_html-css-forms-flexbox-e-responsivo/01_prevent-default/script.js
@@ -2,3 +2,13 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', (event) => event.preventDefault());
+
+INPUT_CHECKBOX.addEventListener('click', (event) => event.preventDefault());
+
+INPUT_TEXT.addEventListener('keypress', (event) => {
+  if (event.key !== 'a') {
+    event.preventDefault();
+  }
+});
