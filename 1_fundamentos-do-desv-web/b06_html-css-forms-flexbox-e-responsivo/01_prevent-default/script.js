35ae6ff9537800a533398e019592e52f36492250
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-04 16:02:50 -0300

    Dia 06.01 - exercícios em andamento

---
+++ 1_fundamentos-do-desv-web/b06_html-css-forms-flexbox-e-responsivo/01_prevent-default/script.js
@@ -0,0 +1,4 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
