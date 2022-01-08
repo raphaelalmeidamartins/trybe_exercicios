2d43d0ecae9a159cf764d69d778ee12fde3f3751
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-08 00:04:37 -0300

    06.01 - Exercícios finais
    Arrumei função de verificar se ano é bissexto

--- 1_fundamentos-do-desv-web/b06_html-css-forms-flexbox-e-responsivo/06.01_html-forms/03_exercicios-finais/script.js
+++ 1_fundamentos-do-desv-web/b06_html-css-forms-flexbox-e-responsivo/06.01_html-forms/03_exercicios-finais/script.js
@@ -99,7 +99,7 @@ function checkDateFormat(date) {
   return false;
 }
 function checkLeapYear(year) {
-  if (year % 4 === 0 || year % 400 === 0) {
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     return true;
   }
   return false;
