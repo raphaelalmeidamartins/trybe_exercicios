bd93ce53154b70f8423b91c188acfaefc0cc3e8e
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-12 16:28:06 -0300

    07.01 - Exercícios finais - Parte I
    Exercício 2 - refatoração.

--- 1_fundamentos-do-desv-web/b07_javascript-es6-let-const-arrow-functions-template-literals/Part-I/ex02.js
+++ 1_fundamentos-do-desv-web/b07_javascript-es6-let-const-arrow-functions-template-literals/Part-I/ex02.js
@@ -1,8 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
-function printSortedArray (array) {
-  return array.sort((a, b) => a - b);
-}
-
-printSortedArray(oddsAndEvens).forEach((x) => console.log(x));
+oddsAndEvens.sort((a, b) => a - b);
+oddsAndEvens.forEach((x) => console.log(x));
