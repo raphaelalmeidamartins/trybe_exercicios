1ebaaeb5b56ea37f15d01e57fa9d71e87ab58861
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-18 16:53:41 -0300

    08.01 - Para fixar
    Concluído.

---
+++ 1_fundamentos-do-desv-web/b08_HOF-JavaScrip-ES6/08.01_introducao-a-Higher-Order-Functions/01_estruturando-HOF/ex01.js
@@ -0,0 +1,7 @@
+const wakeUp = () => 'Acordando!!';
+const haveBreakfast = () => 'Bora tomar café!!';
+const goToBed = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func());
+
+doingThings(wakeUp);
