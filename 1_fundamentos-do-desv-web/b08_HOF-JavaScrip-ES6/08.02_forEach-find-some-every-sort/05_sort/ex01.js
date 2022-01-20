dbe565ac852c4986e172a35b31d617ba2b8b1cd9
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-20 10:57:45 -0300

    08.02 - Para fixar
    some, every e sort concluídos.

---
+++ 1_fundamentos-do-desv-web/b08_HOF-JavaScrip-ES6/08.02_forEach-find-some-every-sort/05_sort/ex01.js
@@ -0,0 +1,13 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+
+people.sort((first, second) => first.age - second.age);
+
+console.log(people);
---
+++ 1_fundamentos-do-desv-web/b08_HOF-JavaScrip-ES6/08.02_forEach-find-some-every-sort/05_sort/ex02.js
@@ -0,0 +1,12 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+people.sort((first, second) => second.age - first.age);
+
+console.log(people);
