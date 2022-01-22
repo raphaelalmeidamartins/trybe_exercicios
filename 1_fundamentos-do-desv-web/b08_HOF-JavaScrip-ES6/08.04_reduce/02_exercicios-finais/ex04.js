623fc1a964379aeef8e06ec5f2d777f39fadbbe8
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-22 17:20:18 -0300

    08.04 - Exercícios finais
    Exercício 4 - concluído.

--- 1_fundamentos-do-desv-web/b08_HOF-JavaScrip-ES6/08.04_reduce/02_exercicios-finais/ex04.js
+++ 1_fundamentos-do-desv-web/b08_HOF-JavaScrip-ES6/08.04_reduce/02_exercicios-finais/ex04.js
@@ -62,3 +62,23 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook(arrayObjects) {
+  // escreva seu código aqui
+  return arrayObjects.reduce((longestName, currentBook) => {
+    return longestName.name.length > currentBook.name.length ? longestName : currentBook;
+  });
+}
+
+console.log(longestNamedBook(books))
