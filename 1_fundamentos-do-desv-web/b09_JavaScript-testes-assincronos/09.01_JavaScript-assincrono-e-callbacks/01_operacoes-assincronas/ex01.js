bcf1e253d76e56c15f59e1527418c471a4473927
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-30 13:42:00 -0300

    09.01 - Iniciei exercícios.

---
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.01_JavaScript-assincrono-e-callbacks/01_operacoes-assincronas/ex01.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  console.log('Comprar parafusos') // Comprar parafusos
+  console.log('Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
---
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.01_JavaScript-assincrono-e-callbacks/01_operacoes-assincronas/ex02.js
@@ -0,0 +1,9 @@
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => console.log(numbers), 3000);
