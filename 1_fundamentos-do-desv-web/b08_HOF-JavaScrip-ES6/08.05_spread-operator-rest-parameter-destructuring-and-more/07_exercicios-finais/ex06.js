f3a5d02c39f545e064f3be46546ba1ea949c7a9e
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-30 14:45:38 -0300

    08.05 - Exercícios finais
    Exercício 6 - concluído.

---
+++ 1_fundamentos-do-desv-web/b08_HOF-JavaScrip-ES6/08.05_spread-operator-rest-parameter-destructuring-and-more/07_exercicios-finais/ex06.js
@@ -0,0 +1,8 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = ([ name, brand, year ]) => ({ name, brand, year });
+
+console.log(toObject(palio));
