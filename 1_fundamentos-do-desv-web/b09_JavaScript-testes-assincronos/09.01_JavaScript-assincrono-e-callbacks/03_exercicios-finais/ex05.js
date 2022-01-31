c02e79daf652474a4f8f40676f88ba5a1a5079fc
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-30 21:42:55 -0300

    09.01 - Exercícios finais
    Exercício 5 - concluído.

---
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.01_JavaScript-assincrono-e-callbacks/03_exercicios-finais/ex05.js
@@ -0,0 +1,22 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  return callback(toFahrenheit(getMarsTemperature()));
+};
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
