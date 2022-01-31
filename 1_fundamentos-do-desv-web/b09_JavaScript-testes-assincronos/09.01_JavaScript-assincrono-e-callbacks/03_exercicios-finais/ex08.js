8b22f3ecf0d7d9df633b7405a14e99d4a54e0eba
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-30 22:48:21 -0300

    09.01 - Exercícios finais
    Exercício 8 - concluído.

---
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.01_JavaScript-assincrono-e-callbacks/03_exercicios-finais/ex08.js
@@ -0,0 +1,41 @@
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+const pokemonFilter = (pokemon) => pokemon.name === 'Charmander';
+const ifError = (error, message) => error ? console.log(error) : console.log(message);
+
+getPokemonDetails(pokemonFilter, ifError);
+
+module.exports = {
+  getPokemonDetails,
+};
