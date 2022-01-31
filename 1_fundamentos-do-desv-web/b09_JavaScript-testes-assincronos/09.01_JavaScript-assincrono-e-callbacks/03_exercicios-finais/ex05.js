3b4f8e2e0470a22f0f35c194c64c6958e27df523
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-30 21:49:00 -0300

    Requisito 9
    Refatoração: corrigi erro.

--- 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.01_JavaScript-assincrono-e-callbacks/03_exercicios-finais/ex05.js
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.01_JavaScript-assincrono-e-callbacks/03_exercicios-finais/ex05.js
@@ -14,8 +14,9 @@ const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
-const sendMarsTemperature = (callback) => {
-  return callback(toFahrenheit(getMarsTemperature()));
+const sendMarsTemperature = (onSuccess) => {
+  const marsTemperature = getMarsTemperature();
+  setTimeout(() => onSuccess(getMarsTemperature())), messageDelay();
 };
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
