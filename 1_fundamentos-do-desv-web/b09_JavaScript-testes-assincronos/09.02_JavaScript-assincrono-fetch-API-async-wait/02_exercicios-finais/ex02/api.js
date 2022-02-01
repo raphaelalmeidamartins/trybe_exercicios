3ef77178a03ba2ccf599d10bee975d9802b05a1c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-01 14:27:44 -0300

    09.02 - Exercícios finais
    Exercício 2 - refatoração: incluí try...catch.

--- 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.02_JavaScript-assincrono-fetch-API-async-wait/02_exercicios-finais/ex02/api.js
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.02_JavaScript-assincrono-fetch-API-async-wait/02_exercicios-finais/ex02/api.js
@@ -1,15 +1,22 @@
 const currenciesList = document.getElementById('currencies-list');
 
 async function getCurrencies() {
-  const arrayCurrencies = await fetch(`https://api.coincap.io/v2/assets`);
-  const response = await arrayCurrencies.json();
-  response.data
-    .filter((_, index) => index < 10)
-    .forEach((currency) => {
-      const newLi = document.createElement('li');
-      newLi.textContent = `${currency.name} (${currency.symbol}): ${currency.priceUsd}`;
-      currenciesList.appendChild(newLi);
-    });
+  try {
+    const arrayCurrencies = await fetch(`https://api.coincap.io/v2/assets`);
+    const response = await arrayCurrencies.json().data;
+    if (!response || response.length === 0) throw new Error('failed to obtain currencies\' information');
+    response
+      .filter((_, index) => index < 10)
+      .forEach((currency) => {
+        const newLi = document.createElement('li');
+        newLi.textContent = `${currency.name} (${currency.symbol}): ${parseFloat(currency.priceUsd.toFixed(2))}`;
+        currenciesList.appendChild(newLi);
+      });    
+  } catch (error) {
+    const newLi = document.createElement('li');
+    newLi.textContent = `Something went wrong: ${error.message}`;
+    currenciesList.appendChild(newLi);
+  }
 }
 
 window.onload = getCurrencies;
