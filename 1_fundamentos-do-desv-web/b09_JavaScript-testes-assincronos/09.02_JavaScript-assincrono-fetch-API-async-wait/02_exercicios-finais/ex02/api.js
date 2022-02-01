80036fac97514bc196be9570538d10a30523cece
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-01 14:41:47 -0300

    09.02 - Exercícios finais
    Exercício 2 - refatorei o código de novo.

--- 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.02_JavaScript-assincrono-fetch-API-async-wait/02_exercicios-finais/ex02/api.js
+++ 1_fundamentos-do-desv-web/b09_JavaScript-testes-assincronos/09.02_JavaScript-assincrono-fetch-API-async-wait/02_exercicios-finais/ex02/api.js
@@ -3,13 +3,12 @@ const currenciesList = document.getElementById('currencies-list');
 async function getCurrencies() {
   try {
     const arrayCurrencies = await fetch(`https://api.coincap.io/v2/assets`);
-    const response = await arrayCurrencies.json().data;
-    if (!response || response.length === 0) throw new Error('failed to obtain currencies\' information');
-    response
+    const response = await arrayCurrencies.json();
+    response.data
       .filter((_, index) => index < 10)
       .forEach((currency) => {
         const newLi = document.createElement('li');
-        newLi.textContent = `${currency.name} (${currency.symbol}): ${parseFloat(currency.priceUsd.toFixed(2))}`;
+        newLi.textContent = `${currency.name} (${currency.symbol}): ${Number(currency.priceUsd).toFixed(2)}`;
         currenciesList.appendChild(newLi);
       });    
   } catch (error) {
