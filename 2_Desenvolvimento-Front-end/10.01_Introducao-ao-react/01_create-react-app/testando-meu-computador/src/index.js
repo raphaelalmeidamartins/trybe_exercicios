29638bb7c9f49349e4553c3a292f5a23a143ffed
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-10 01:12:33 -0300

    10.01 - Para fixar
    Usei Hello World.

--- 2_Desenvolvimento-Front-end/10.01_Introducao-ao-react/01_create-react-app/testando-meu-computador/src/index.js
+++ 2_Desenvolvimento-Front-end/10.01_Introducao-ao-react/01_create-react-app/testando-meu-computador/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString('pt', { hour12: true })}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
