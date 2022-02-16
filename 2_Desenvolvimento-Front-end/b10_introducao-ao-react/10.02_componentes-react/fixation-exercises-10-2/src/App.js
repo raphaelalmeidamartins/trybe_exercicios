1badb6983f1699fac1f0ab70082591140623f1ca
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-16 19:14:51 -0300

    10.02 - Para fixar
    Composição de componentes - em andamento.

--- 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/App.js
+++ 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
-import Image from './Image'
+import Order from './Order';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
+        <h1> Orders recently created </h1>
+        <Order order={headphone} />
+        <Order order={energyDrink} />
       </div>
     );
   }
---
+++ 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/Order.jsx
@@ -0,0 +1,16 @@
+// arquivo Order.js
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    const { user, product, price } = this.props.order;
+
+    return (
+      <div className="order">
+        <p> {user} bought {product} for {price.value} {price.currency} </p>
+      </div>
+    );
+  }
+}
+
+export default Order;
