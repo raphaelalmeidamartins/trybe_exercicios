4a3058d326a008417f1c50b64b7ed80a4be984b2
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-16 15:35:14 -0300

    10.01 - Para fixar
    React e classes - concluído.

--- 2_Desenvolvimento-Front-end/10.01_Introducao-ao-react/02_classes-e-react/about-me/src/App.js
+++ 2_Desenvolvimento-Front-end/10.01_Introducao-ao-react/02_classes-e-react/about-me/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import picOfMe from './picOfMe.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>Raphael Martins</h1>
+        <img src={picOfMe} alt="Minha foto"></img>
+        <div>
+          <p>I am a 27-year-old Brazilian who lives in São Paulo, Brazil. I am a nerd boy who loves technology, pop music, comic books, and games. Nowadays, I am studying Web Development at Trybe, and I am very excited about this new chapter of my life. I also want to learn mobile development soon.</p>
+          <p>I am a very creative person who likes to take on challenges and solve problems. Technology is a powerful tool that can change the world for the better. I am very dynamic, and I don’t like to be stagnant. If something is bothering me in both my personal and professional lives, I will actively be searching for solutions. I prefer to be around people with a growth mindset.</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
