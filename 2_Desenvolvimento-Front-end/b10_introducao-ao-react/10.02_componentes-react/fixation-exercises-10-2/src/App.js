f73e4c4eaa69172a64262dea2ad35b75e2ee7b7f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-16 19:18:54 -0300

    10.02 - Para fixar
    Composição de componentes - concluído.

--- 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/App.js
+++ 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,35 +1,35 @@
 // arquivo App.js, criado pelo create-react-app, modificado
-import React from 'react';
-import './App.css';
-import Order from './Order';
+import React from "react";
+import "./App.css";
+import Order from "./Order";
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
-      }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+    const ordersArray = [
+      {
+        id: 102,
+        user: "cena@gmail.com",
+        product: "Razer Headphone",
+        price: {
+          value: 99.99,
+          currency: "dollars",
+        },
+      },
+      {
+        id: 77,
+        user: "cena@gmail.com",
+        product: "Monster 500mL",
+        price: {
+          value: 9.99,
+          currency: "dollars",
+        },
+      },
+    ];
 
     return (
       <div className="App">
         <h1> Orders recently created </h1>
-        <Order order={headphone} />
-        <Order order={energyDrink} />
+        {ordersArray.map((order) => <Order order={order} />)}
       </div>
     );
   }
