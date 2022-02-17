a9fd50f30bd26b5e44b74fb082f91407d797e15f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-02-17 15:21:43 -0300

    10.02 - Para fixar
    Listas de componentes e chaves - concluído.

--- 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/App.js
+++ 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,35 +1,28 @@
 // arquivo App.js, criado pelo create-react-app, modificado
-import React from "react";
-import "./App.css";
-import Order from "./Order";
+import React from 'react';
+import './App.css';
+import UserProfile from './UserProfile';
 
 class App extends React.Component {
   render() {
-    const ordersArray = [
+    const users = [
       {
         id: 102,
-        user: "cena@gmail.com",
-        product: "Razer Headphone",
-        price: {
-          value: 99.99,
-          currency: "dollars",
-        },
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
       },
       {
         id: 77,
-        user: "cena@gmail.com",
-        product: "Monster 500mL",
-        price: {
-          value: 9.99,
-          currency: "dollars",
-        },
-      },
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      }
     ];
 
     return (
       <div className="App">
-        <h1> Orders recently created </h1>
-        {ordersArray.map((order) => <Order order={order} />)}
+        {users.map((user, index) => <UserProfile key={index} user={user} />)}
       </div>
     );
   }
---
+++ 2_Desenvolvimento-Front-end/b10_introducao-ao-react/10.02_componentes-react/fixation-exercises-10-2/src/UserProfile.jsx
@@ -0,0 +1,17 @@
+// arquivo UserProfile.js
+import React from 'react';
+import Image from './Image';
+
+class UserProfile extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> {this.props.user.name} </p>
+        <p> {this.props.user.email} </p>
+        <Image source={this.props.user.avatar} alternativeText="User avatar" />
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
