893295c6807452339b3a8503a2e21b284f6d2eca
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-03-21 23:24:55 -0300

    14.02 - Para fixar (concluído).

---
+++ 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/my-react-project-api-test/src/App.test.js
@@ -0,0 +1,28 @@
+// App.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+afterEach(() => jest.clearAllMocks());
+
+it('fetches a joke', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
+  render(<App />);
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
+});
