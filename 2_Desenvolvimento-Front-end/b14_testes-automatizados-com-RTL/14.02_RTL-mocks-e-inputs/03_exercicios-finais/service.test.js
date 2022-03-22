edb573617eeec075f1f6ee4eae787a88ea722afa
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-03-22 00:56:35 -0300

    14.02 - exercicios finais - exercício 3 (concluído)

--- 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/03_exercicios-finais/service.test.js
+++ 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/03_exercicios-finais/service.test.js
@@ -17,4 +17,16 @@ test('Define novo mock para função generateRandomNum', () => {
   expect(mockGenerateRandomNum(10, 2)).toBe(5);
   expect(mockGenerateRandomNum).toHaveBeenCalled();
   expect(mockGenerateRandomNum).toHaveBeenCalledTimes(3);
+  expect(mockGenerateRandomNum).toHaveBeenCalledWith(10, 2);
+});
+
+test('Define mais um novo mock para função generateRandomNum', () => {
+  const mockGenerateRandomNum = jest
+    .spyOn(service, 'generateRandomNum')
+    .mockImplementation((a, b, c) => a * b * c);
+
+  expect(mockGenerateRandomNum(10, 2, 2)).toBe(40);
+  expect(mockGenerateRandomNum).toHaveBeenCalled();
+  expect(mockGenerateRandomNum).toHaveBeenCalledTimes(4);
+  expect(mockGenerateRandomNum).toHaveBeenCalledWith(10, 2);
 });
