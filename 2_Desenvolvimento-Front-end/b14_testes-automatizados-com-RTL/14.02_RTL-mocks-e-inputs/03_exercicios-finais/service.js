06d371bd95676907311dd3f1b5176b17d572296e
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-03-25 12:26:18 -0300

    14.02 - Exercícios finais - concluído.

--- 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/03_exercicios-finais/service.js
+++ 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/03_exercicios-finais/service.js
@@ -1,3 +1,17 @@
 const generateRandomNum = () => Math.floor(Math.random() * 101);
+const toUpperCaseString = (string) => string.toUpperCase();
+const returnFirstCharacter = (string) => string.slice(0, 1);
+const concatStrings = (string1, string2) => string1 + string2;
+const fetchRandomDoguinho = async () => {
+  return fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((data) => data.ok ? Promise.resolve(data) : Promise.reject(data));
+};
 
-module.exports = { generateRandomNum };
+module.exports = {
+  generateRandomNum,
+  toUpperCaseString,
+  returnFirstCharacter,
+  concatStrings,
+  fetchRandomDoguinho,
+ };
--- 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/03_exercicios-finais/service.test.js
+++ 2_Desenvolvimento-Front-end/b14_testes-automatizados-com-RTL/14.02_RTL-mocks-e-inputs/03_exercicios-finais/service.test.js
@@ -30,3 +30,60 @@ test('Define mais um novo mock para função generateRandomNum', () => {
   expect(mockGenerateRandomNum).toHaveBeenCalledTimes(4);
   expect(mockGenerateRandomNum).toHaveBeenCalledWith(10, 2);
 });
+
+const mockToUpperCaseString = jest
+  .spyOn(service, 'toUpperCaseString')
+  .mockImplementation((string) => string.toLowerCase());
+
+const mockReturnFirstCharacter = jest
+  .spyOn(service, 'returnFirstCharacter')
+  .mockImplementation((string) => string.slice(string.length - 1, string.length));
+
+const mockConcatStrings = jest
+  .spyOn(service, 'concatStrings')
+  .mockImplementation((string1, string2, string3) => string1 + string2 + string3);
+
+test('Verifica o mock da toUpperCaseString', () => {
+  expect(mockToUpperCaseString('RAPHA')).toBe('rapha');
+  expect(mockToUpperCaseString).toHaveBeenCalled();
+  expect(mockToUpperCaseString).toHaveBeenCalledTimes(1);
+  expect(mockToUpperCaseString).toHaveBeenCalledWith('RAPHA');
+});
+
+test('Verifica o mock da mockReturnFirstCharacter', () => {
+  expect(mockReturnFirstCharacter('RAPHA')).toBe('A');
+  expect(mockReturnFirstCharacter).toHaveBeenCalled();
+  expect(mockReturnFirstCharacter).toHaveBeenCalledTimes(1);
+  expect(mockReturnFirstCharacter).toHaveBeenCalledWith('RAPHA');
+});
+
+test('Verifica o mock da mockConcatStrings', () => {
+  expect(mockConcatStrings('rapha', ' almeida', ' martins')).toBe('rapha almeida martins');
+  expect(mockConcatStrings).toHaveBeenCalled();
+  expect(mockConcatStrings).toHaveBeenCalledTimes(1);
+  expect(mockConcatStrings).toHaveBeenCalledWith('rapha', ' almeida', ' martins');
+});
+
+test('Verifica implementação original', () => {
+  service.toUpperCaseString.mockRestore();
+  expect(service.toUpperCaseString('rapha')).toBe('RAPHA');
+});
+
+service.fetchRandomDoguinho = jest.fn();
+afterEach(service.fetchRandomDoguinho.mockReset);
+
+test('Testando requisição caso a promisse resolva', async () => {
+  service.fetchRandomDoguinho.mockResolvedValue('sucess');
+
+  await expect(service.fetchRandomDoguinho()).resolves.toBe('sucess');
+  expect(service.fetchRandomDoguinho).toHaveBeenCalled();
+  expect(service.fetchRandomDoguinho).toHaveBeenCalledTimes(1);
+});
+
+test('Testando requisição caso seja rejeitada', async () => {
+  service.fetchRandomDoguinho.mockRejectedValue('failed');
+
+  await expect(service.fetchRandomDoguinho()).rejects.toBe('failed');
+  expect(service.fetchRandomDoguinho).toHaveBeenCalled();
+  expect(service.fetchRandomDoguinho).toHaveBeenCalledTimes(1);
+})
