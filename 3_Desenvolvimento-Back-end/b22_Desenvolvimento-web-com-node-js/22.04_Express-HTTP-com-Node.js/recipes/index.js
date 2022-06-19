1df52cc26d3df4b2b49b3bf9d8b44cfb500ba2eb
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-06-19 17:58:36 -0300

    22.04 - Exercícios de fixação de Express (roteamento, parametros e query string) concluídos

---
+++ 3_Desenvolvimento-Back-end/b22_Desenvolvimento-web-com-node-js/22.04_Express-HTTP-com-Node.js/recipes/index.js
@@ -0,0 +1,17 @@
+/* index.js */
+const express = require('express');
+const app = express();
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
