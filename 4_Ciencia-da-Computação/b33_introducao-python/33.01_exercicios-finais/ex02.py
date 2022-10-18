c36d2fe2d8fb765535de6a212b57a389fe4d8a30
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-10-17 21:31:02 -0300

    33.01 - Exercícios finais
    Ex02 - concluído

---
+++ 4_Ciencia-da-Computação/b33_introducao-python/33.01_exercicios-finais/ex02.py
@@ -0,0 +1,9 @@
+def listAverage(listNumbers: list[float]) -> float:
+  result = 0;
+
+  for number in listNumbers:
+    result += number;
+  
+  return result / len(listNumbers);
+
+print(listAverage([1, 2.5, 3, 4, 5]));
